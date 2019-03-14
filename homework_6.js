26fe190465dfd5cc949352f2991cdb0f06d44d9c
Ivica Sirjan <isirjan@gmail.com>
2019-03-14 01:51:39 +0100

    Add hallArray

---
+++ homework_6.js
@@ -0,0 +1,7 @@
+var hallArray = [
+    {id:1, name: "Velika sportska dvorana", reservation: {isReserved: false, reservedFrom: null, reservedUntill: null}},
+    {id:2, name: "Dvorana Hrvatskog doma", reservation: {isReserved: false, reservedFrom: null, reservedUntill: null}},
+    {id:3, name: "Dvorana Gradske knjižnice", reservation: {isReserved: false, reservedFrom: null, reservedUntill: null}},
+    {id:4, name: "Dvorana TIC (Turistički dom)", reservation: {isReserved: false, reservedFrom: null, reservedUntill: null}},
+    {id:5, name: "Konferencijske dvorane RCTP (Razvojni centar i tehnološki park)", reservation: {isReserved: false, reservedFrom: null, reservedUntill: null}}
+];
