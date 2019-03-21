8c50d5ba169e1618a40583f72bb4e3902a916330
Ivica Sirjan <isirjan@gmail.com>
2019-03-21 16:49:33 +0100

    Solved Tasks 4; Create Hall array object

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+const hallArray = [
+  {id:1, name: "Velika sportska dvorana", reservation: {isReserved: true, reservedFrom: '2019-03-02T23:00:00.000Z', reservedUntill: '2019-03-30T23:00:00.000Z'}},
+  {id:2, name: "Dvorana Hrvatskog doma", reservation: {isReserved: true, reservedFrom: '2019-04-02T23:00:00.000Z', reservedUntill: '2019-04-30T23:00:00.000Z'}},
+  {id:3, name: "Dvorana Gradske knjižnice", reservation: {isReserved: false, reservedFrom: null, reservedUntill: null}},
+  {id:4, name: "Dvorana TIC (Turistički dom)", reservation: {isReserved: false, reservedFrom: null, reservedUntill: null}},
+  {id:5, name: "Konferencijske dvorane RCTP (Razvojni centar i tehnološki park)", reservation: {isReserved: false, reservedFrom: null, reservedUntill: null}}
+];
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
