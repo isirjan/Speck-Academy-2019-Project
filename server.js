64960f3ea442a050c6aa64fb814e495cd98ae1a3
Ivica Sirjan <isirjan@gmail.com>
2019-03-22 00:23:31 +0100

    Solved an corrected HW-9; Add functions for Hall names array and string

--- server.js
+++ server.js
@@ -24,24 +24,6 @@ function getHallNamesString () {
   return hallNamesString;
 }
 
-// function getHallNamesString () {
-//   const hallNamesString = '';
-//   for (i = 0; i < getHallNames().length; i++) {
-//     hallNamesString = getHallNames()[i] + ", ";
-//   }
-//   return hallNamesString;
-// }
-
-// const hallNames = [
-//   "Velika sportska dvorana",
-//   "Dvorana Hrvatskog doma",
-//   "Dvorana Gradske knjižnice",
-//   "Dvorana Gradske knjižnice",
-//   "Konferencijske dvorane RCTP (Razvojni centar i tehnološki park)"
-// ]
-
-// const hallNamesString = "Velika sportska dvorana, Dvorana Hrvatskog doma, Dvorana Gradske knjižnice, Dvorana Gradske knjižnice, Konferencijske dvorane RCTP (Razvojni centar i tehnološki park)"
-
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
@@ -51,10 +33,3 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-
-// z_10 zadatak3
-// const halls = 'halls';
-
-// module.exports = {
-//   halls
-// }
