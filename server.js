b66ad1853d30a73b322ac67ad755f8344ae263d8
Ivica Sirjan <isirjan@gmail.com>
2019-04-09 19:03:21 +0200

    Merged all changes to master

--- server.js
+++ server.js
@@ -1,35 +1,5 @@
 const http = require('http');
 
-<<<<<<< HEAD
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const hallArray = [
-  {id:1, name: "Velika sportska dvorana", reservation: {isReserved: true, reservedFrom: new Date('2019-03-02'), reservedUntill: new Date('2019-03-30')}},
-  {id:2, name: "Dvorana Hrvatskog doma", reservation: {isReserved: true, reservedFrom: new Date('2019-04-02'), reservedUntill: new Date('2019-04-30')}},
-  {id:3, name: "Dvorana Gradske knjižnice", reservation: {isReserved: false, reservedFrom: null, reservedUntill: null}},
-  {id:4, name: "Dvorana TIC (Turistički dom)", reservation: {isReserved: false, reservedFrom: null, reservedUntill: null}},
-  {id:5, name: "Konferencijske dvorane RCTP (Razvojni centar i tehnološki park)", reservation: {isReserved: false, reservedFrom: null, reservedUntill: null}}
-];
-
-function getHallNames () {
-  const hallNames = [];
-  for (i = 0; i < hallArray.length; i++) {
-    hallNames.push(hallArray[i].name);
-  }
-  return hallNames;
-}
-
-function getHallNamesString () {
-  const hallNamesString = getHallNames().join(", ");
-  return hallNamesString;
-}
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end(getHallNamesString());
-=======
 const ha = require('./halls.js');
 
 const hostname = '127.0.0.1';
@@ -40,7 +10,6 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.write('Broj dvorana je: ' + ha.hallArray.length);
   res.end();
->>>>>>> homework-10
 });
 
 server.listen(port, hostname, () => {
